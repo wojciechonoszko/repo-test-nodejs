574baf016c29dbf9dc93ac6b16c76d8f5f3c0a1f
wojciechonoszko <wojciechonoszko@gmail.com>
2021-12-13 11:19:36 +0100

    add files

---
+++ src/js/slides.js
@@ -0,0 +1,13 @@
+jQuery(function() {
+    $('.reviews').slick({
+        arrows: false,
+        dots: true,
+        speed: 1000,
+        easing: 'ease',
+        autoplay: true,
+        autoplaySpeed: 10000,
+        pauseOnFocus: true,
+        pauseOnHover: true,
+        pauseOnDotsHover: true,
+    });
+});
